7fdf0eed0bce3c41174239cdc52e567a945dd267
Wilson <wilsonvillegas@rocketmail.com>
2014-07-20 23:27:32 -0500

    Initial commit-working platformer

---
+++ spacecarnival/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour 
+{
+	private Transform target;
+	private float trackingSpeed = 4;
+
+	public void SetTarget(Transform t)
+	{
+		target = t;
+	}
+
+	void LateUpdate()
+	{
+		if (target)
+		{
+			float x = Accelerate (transform.position.x, target.position.x, trackingSpeed);
+			float y = Accelerate (transform.position.y, target.position.y, trackingSpeed);
+			transform.position = new Vector3(x, y, transform.position.z);
+		}
+	}
+
+	private float Accelerate(float n, float target, float a)
+	{
+		if (n == target)
+		{
+			return n;
+		}
+		else
+		{
+			//n gets faster or slower, whichver goes closer to target speed
+			float dir = Mathf.Sign(target - n);
+			n += a * Time.deltaTime * dir;
+			//If n has passed the target, return the target speed, otherwise return n
+			return (dir == Mathf.Sign (target-n))? n: target;
+		}
+	}
+
+
+}
---
+++ spacecarnival/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent (typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+	
+	public LayerMask collisionMask;
+	
+	private BoxCollider collider;
+	private Vector3 size;
+	private Vector3 center;
+	
+	private float skin = .005f;
+	
+	[HideInInspector]
+	public bool grounded;
+	[HideInInspector]
+	public bool movementStopped;
+	
+	Ray ray;
+	RaycastHit hit;
+	
+	void Start() {
+		collider = GetComponent<BoxCollider>();
+		size = collider.size;
+		center = collider.center;
+	}
+	
+	public void Move(Vector2 moveAmount) {
+		
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 p = transform.position;
+		
+		// Check vertical collisions
+		grounded = false;
+		
+		for (int i = 0; i<3; i ++) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + center.x - size.x/2) + size.x/2 * i; // Left, center, and right edge of the collider
+			float y = p.y + center.y + size.y/2 * dir; // Bottom of collider
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+			Debug.DrawRay(ray.origin,ray.direction);
+			
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaY) + skin,collisionMask)) {
+				// Distance between the player and the ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				
+				// Stop player's downwards movement after coming within skin width of a collider
+				if (dst > skin) {
+					deltaY = dst * dir - skin * dir;
+				}
+				else {
+					deltaY = 0;
+				}
+				
+				grounded = true;
+				
+				break;
+				
+			}
+		}
+		
+		
+		// Check collisions left and right
+		movementStopped = false;
+		for (int i = 0; i<3; i ++) {
+			float dir = Mathf.Sign(deltaX);
+			float x = p.x + center.x + size.x/2 * dir;
+			float y = p.y + center.y - size.y/2 + size.y/2 * i;
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(dir,0));
+			Debug.DrawRay(ray.origin,ray.direction);
+			
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaX) + skin,collisionMask)) {
+				// Distance between player and ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				
+				// Stop player's downwards movement after coming within skin width of a collider
+				if (dst > skin) {
+					deltaX = dst * dir - skin * dir;
+				}
+				else {
+					deltaX = 0;
+				}
+				
+				movementStopped = true;
+				break;
+				
+			}
+		}
+		
+		
+		
+		
+		Vector2 finalTransform = new Vector2(deltaX,deltaY);
+		
+		transform.Translate(finalTransform);
+	}
+	
+}
